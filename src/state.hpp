b29fa7cd521e934ac871e3b8b943e6c822dd0e53
Warren W. Kretzschmar <wkretzsch@gmail.com>
2015-06-27 12:07:47 +0100

    Adds more tests for state

--- src/state.hpp
+++ src/state.hpp
@@ -5,6 +5,7 @@
 #define _STATE_HPP 1
 
 #include <stdexcept>
+#include <string>
 #include <vector>
 
 class State {
@@ -19,6 +20,17 @@ public:
   State() { m_fields.resize(9, 0); }
 
   unsigned getField(unsigned field) const { return m_fields.at(field); }
+  void setField(unsigned field, unsigned value) {
+    if (value > 2)
+      throw std::runtime_error("Attempted to set field to invalid value: " +
+                               std::to_string(value));
+    if (field > 8)
+      throw std::range_error("Tried to set non-existent field: " +
+                             std::to_string(field));
+    if (m_fields[field])
+      throw std::runtime_error("A field can only be set once");
+    m_fields[field] = static_cast<unsigned char>(value);
+  }
 };
 
 #endif /* _STATE_HPP */
--- testing/state_ut.cpp
+++ testing/state_ut.cpp
@@ -5,6 +5,8 @@
 
 // tests for the state object
 
+using namespace std;
+
 // make sure the state object builds and is initialized properly
 TEST(StateClass, builds) {
 
@@ -12,4 +14,19 @@ TEST(StateClass, builds) {
   for (size_t i = 0; i < 9; i++) {
     EXPECT_EQ(0, state1.getField(i));
   }
+}
+
+TEST(StateClass, isChangeable) {
+
+  State state;
+  state.setField(0, 1);
+
+  // make sure that fields can only be set once
+  for (size_t i = 0; i < 3; i++) {
+    ASSERT_THROW(state.setField(0, i), runtime_error);
+  }
+
+  // set another field
+  state.setField(2, 2);
+  
 }
