e1005fc784a07bb27f6d558a609f7759a95deed8
Warren W. Kretzschmar <wkretzsch@gmail.com>
2015-06-27 16:35:48 +0100

    Adds tests for actVal

--- testing/actVal_ut.cpp
+++ testing/actVal_ut.cpp
@@ -16,8 +16,29 @@ TEST(ActValClass, builds) {
 }
 
 // let's try to update and retrieve some action values
-TEST(ActValClass, updates){
+TEST(ActValClass, updates) {
 
-    ActValFunc actVal;
-    State state;
+  ActValFunc actVal;
+  State state;
+
+  for (size_t i = 0; i < 9; ++i) {
+    actVal.setVal(state, i, 1, i);
+    EXPECT_EQ(i, actVal.getVal(state, i, 1));
+  }
+
+  // make a move and see what new action value pairs are available
+  // and settable
+  state.setField(0, 1);
+  for (size_t i = 0; i < 9; ++i) {
+    ASSERT_THROW(actVal.getVal(state, i, 1), runtime_error);
+
+    if (i == 0) {
+      ASSERT_THROW(actVal.getVal(state, i, 2), runtime_error);
+
+    } else {
+      EXPECT_EQ(0, actVal.getVal(state, i, 2));
+      actVal.setVal(state, i, 2, i * 2);
+      EXPECT_EQ(i * 2, actVal.getVal(state, i, 2));
+    }
+  }
 }
